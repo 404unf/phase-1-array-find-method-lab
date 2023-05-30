28bca830d6bdd409f5ce3d11a57afc3ec139dbdb
Anna <34191811+404unf@users.noreply.github.com>
2023-05-29 22:55:33 -0700

    Array: find item in object

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+
+function superbowlWin(record) {
+    const isWin = item => item.result === 'W'
+    const founditem = record.find(isWin)
+    if (founditem) {return founditem.year}
+    else {return founditem}
+}
